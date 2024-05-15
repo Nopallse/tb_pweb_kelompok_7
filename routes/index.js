a9015ff82bead1292876a7ff3c36d573c5153da0
nopallse <noppal.901@gmail.com>
2024-05-15 10:59:35 +0700

    oke

--- routes/index.js
+++ routes/index.js
@@ -1,92 +1,23 @@
 import express from "express";
-import session from "express-session";
-import { getUsers, Register, Login, Logout } from "../controllers/auth.js";
-import { verifyToken } from "../middleware/VerifyToken.js";
+import { Login, Logout } from "../controllers/auth.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
-import { checkUserLoggedIn } from "../controllers/auth.js";
-import checkRole from "../middleware/checkrole.middleware.js";
 const router = express.Router();
 
-router.use(session({
-  secret: 'secret_key', 
-  resave: false,
-  saveUninitialized: true
-}));
-
-// Endpoint untuk mendapatkan data pengguna
-router.get('/users', verifyToken, async (req, res) => {
-  try {
-    // Ambil data pengguna dari database
-    const users = await getUsers(); 
-
-    // Kirim data pengguna sebagai respons
-    res.json(users);
-  } catch (error) {
-
-    console.error('Error fetching user data:', error);
-
-    res.status(500).send('Internal Server Error');
-  }
-});
 
 
-router.get('/admin', (req, res) => {
-  res.redirect('/admin/dashboard'); // Redirect ke halaman home
-});
-
-
-
-// Endpoint untuk registrasi pengguna baru
-router.post('/users', Register);
-
-// Endpoint untuk login
 router.post('/login', Login);
 
-// Endpoint untuk mendapatkan refresh token baru
-router.get('/token', refreshToken);
-
-// Endpoint untuk logout
-router.delete('/logout', Logout);
-
-router.get('/', (req, res) => {
-  res.redirect('/home'); // Redirect ke halaman home
-});
-
-
-
-
-// // Route untuk halaman home
-// router.get('/home', async (req, res) => {
-//   const { userLoggedIn, user } = checkUserLoggedIn(req);
-//   res.render('user/home', { userLoggedIn, user });
-// });
-
-router.get("/home", verifyToken, checkRole("mahasiswa"), async (req, res) => {
-  const {  user } = checkUserLoggedIn(req);
-  res.render("user/home",{ user });
-});
-
-
-
-
-
-
-// Route untuk halaman login
-router.get('/login', (req, res) => {
+router.get('/login', (req,res) => {
   res.render('login');
 });
 
-// Route untuk logout
-router.get('/logout', (req, res) => {
-  // Hapus cookie refreshToken
-  res.clearCookie('refreshToken');
 
-  // Redirect kembali ke halaman utama
+router.delete('/logout', Logout);
+
+router.get('/logout', (req, res) => {
+  res.clearCookie('refreshToken');
   res.redirect('/login');
 });
 
-router.get('/account', verifyToken, async (req, res) => {
-  res.send('Token valid, akses ke rute /account berhasil.');
-});
 
 export default router;
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,81 +1,55 @@
 import express from "express";
 import { verifyToken } from "../middleware/VerifyToken.js";
-import { checkUserLoggedIn, editProfile, view_profile } from "../controllers/auth.js";
+import { editProfile, getUser } from "../controllers/auth.js";
 import { changePassword} from "../controllers/auth.js";
 
 const router = express.Router();
 
 
 router.get('/', (req, res) => {
-  res.redirect('/home'); // Redirect ke halaman home
-});
-// Route untuk halaman home
-
-
-
-
-router.get('/home', async (req, res) => {
-  const {  user } = checkUserLoggedIn(req);
-  res.render('home', {  user });
+  res.redirect('/home'); 
 });
 
 
+router.get("/home", verifyToken('mahasiswa'), async function (req, res) {
+    const user = await getUser(req, res); 
+    res.render("user/home", { user });
+});
+
+
+router.get("/profile", verifyToken('mahasiswa'), async function (req, res) {
+  const user = await getUser(req, res); 
+  res.render("user/profile", { user });
+});
 
 
 
-
-
-router.get('/profile',verifyToken, view_profile );
-
-
-
-router.get('/profile/change-password',verifyToken, async (req, res) => {
-  const { user } = checkUserLoggedIn(req);
+router.get('/profile/change-password',verifyToken('mahasiswa'), async function (req, res) {
+  const user = await getUser(req, res); 
   res.render('user/change-password', { user });
 });
 
-// Route untuk mengganti password pengguna
-router.post('/change-password', verifyToken, async (req, res) => {
-  try {
-    await changePassword(req, res);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Terjadi kesalahan server" });
-  }
+router.post('/change-password', verifyToken('mahasiswa'), async (req, res) => {
+  await changePassword(req, res);
 });
 
 
-
-
-
-
-// Route untuk mengganti password pengguna
-router.post('/change-password', verifyToken, async (req, res) => {
-  try {
-    await changePassword(req, res);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Terjadi kesalahan server" });
-  }
+router.get('/logout', (req, res) => {
+  res.clearCookie('refreshToken');
+  res.redirect('/login');
 });
 
+router.post('/change-password', verifyToken, async (req, res) => {
+  await changePassword(req, res);
+});
 
-router.get('/profile/change-profile',verifyToken, async (req, res) => {
-  const { userLoggedIn, user } = checkUserLoggedIn(req);
+router.get('/profile/change-profile',verifyToken('mahasiswa'), async (req, res) => {
+  const user = await getUser(req, res); 
   res.render('user/change-profile', { user });
 });
 
-
-
-
-// Route untuk mengganti password pengguna
-router.post('/change-profile', verifyToken, async (req, res) => {
-  try {
-    await editProfile(req, res);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Terjadi kesalahan server" });
-  }
+router.post('/change-profile', verifyToken('mahasiswa'), async (req, res) => {
+  await editProfile(req, res);
 });
 
   export default router;
