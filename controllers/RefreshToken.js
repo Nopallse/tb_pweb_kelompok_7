33021e939e4e9267d9817e1ae1a01c1879c87834
nopallse <noppal.901@gmail.com>
2024-05-15 10:36:23 +0700

    perbaikan

--- controllers/RefreshToken.js
+++ controllers/RefreshToken.js
@@ -5,42 +5,37 @@ export const refreshToken = async (req, res) => {
   try {
     const refreshToken = req.cookies.refreshToken;
     if (!refreshToken) return res.sendStatus(401);
-    console.log('Refresh Token:', refreshToken); 
+
+    console.log('Received refresh token:', refreshToken);
 
     const user = await Users.findOne({
-      where: {
-        refresh_token: refreshToken,
-      },
+      where: { refresh_token: refreshToken },
     });
-    if (!user[0]) { 
-        console.log('User tidak ditemukan dengan refresh token tersebut'); 
-        return res.sendStatus(403);
-    }
-    
-    console.log('User ditemukan:', user[0]);
 
-    jwt.verify(
-      refreshToken,
-      process.env.REFRESH_TOKEN_SECRET,
-      (err, decoded) => {
-        if (err){ 
-            console.log('Verifikasi refresh token gagal:', err);
-            return res.sendStatus(403);
-        }
-        const userId = user[0].id;
-        const name = user[0].name;
-        const email = user[0].email;
-        const accessToken = jwt.sign(
-          { userId, name, email },
-          process.env.ACCESS_TOKEN_SECRET,
-          {
-            expiresIn: "10s",
-          }
-        );
-        res.json({ accessToken });
+    if (!user) {
+      console.log('User tidak ditemukan dengan refresh token tersebut');
+      return res.sendStatus(403);
+    }
+
+    console.log('User ditemukan:', user);
+
+    jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        console.log('Verifikasi refresh token gagal:', err);
+        return res.sendStatus(403);
       }
-    );
+
+      const { userId, name, email, role, nim, hp, departemen } = decoded;
+      const newAccessToken = jwt.sign({ userId, name, email, role, nim, hp, departemen }, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "10s",
+      });
+
+      console.log('New access token generated:', newAccessToken);
+
+      res.json({ accessToken: newAccessToken });
+    });
   } catch (error) {
-    console.log(error);
+    console.error("Refresh token error:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
