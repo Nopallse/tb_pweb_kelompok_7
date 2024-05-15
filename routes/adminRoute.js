aeb6583ad493a1a64c80edf5f5ff607de0bfc0c4
nopallse <noppal.901@gmail.com>
2024-05-15 11:04:53 +0700

    oke

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -1,55 +1,25 @@
 import express from "express";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import { editProfile, getUser } from "../controllers/auth.js";
-import { changePassword} from "../controllers/auth.js";
 
 const router = express.Router();
 
-
 router.get('/', (req, res) => {
-  res.redirect('/home'); 
+  res.redirect('/admin/dashboard');
 });
 
-
-router.get("/home", verifyToken('mahasiswa'), async function (req, res) {
-    const user = await getUser(req, res); 
-    res.render("user/home", { user });
-});
-
-
-router.get("/profile", verifyToken('mahasiswa'), async function (req, res) {
+router.get("/dashboard", verifyToken('admin'), async (req, res) => {
   const user = await getUser(req, res); 
-  res.render("user/profile", { user });
+  res.render("admin/dashboard",{  user });
 });
 
-
-
-router.get('/profile/change-password',verifyToken('mahasiswa'), async function (req, res) {
+router.get("/profile", verifyToken('admin'), async (req, res) => {
   const user = await getUser(req, res); 
-  res.render('user/change-password', { user });
-});
-
-router.post('/change-password', verifyToken('mahasiswa'), async (req, res) => {
-  await changePassword(req, res);
+  res.render("admin/profile",{  user });
 });
 
 
-router.get('/logout', (req, res) => {
-  res.clearCookie('refreshToken');
-  res.redirect('/login');
-});
 
-router.post('/change-password', verifyToken, async (req, res) => {
-  await changePassword(req, res);
-});
 
-router.get('/profile/change-profile',verifyToken('mahasiswa'), async (req, res) => {
-  const user = await getUser(req, res); 
-  res.render('user/change-profile', { user });
-});
 
-router.post('/change-profile', verifyToken('mahasiswa'), async (req, res) => {
-  await editProfile(req, res);
-});
-
-  export default router;
+export default router;
